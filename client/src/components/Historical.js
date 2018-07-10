248b7470bf48489deeac2c458e984cffa7c0222e
student <leonlau95@gmail.com>
2018-07-10 12:55:43 +0800

    added form, historical, main

---
+++ client/src/components/Historical.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Historical extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Historical works</h1>
+      </div>
+    );
+  }
+}
+
+export default Historical;
